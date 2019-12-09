d529a355f2d1a2fd80bc4ae8160ecb8fbdf7c003
NgocDinh <ngocdt2@smartosc.com>
2019-12-09 10:28:12 +0700

    Updates

---
+++ precache-manifest.0d655f3df6bb90b433a00dc51a65e4c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a2ef786664391227ed3d7394bbeb941",
+    "url": "/webchat/index.html"
+  },
+  {
+    "revision": "c0d2ec6040188ad261c6",
+    "url": "/webchat/static/css/main.f206fe27.chunk.css"
+  },
+  {
+    "revision": "0cb6c7aed5c8d5f4b703",
+    "url": "/webchat/static/js/2.c055dd42.chunk.js"
+  },
+  {
+    "revision": "c0d2ec6040188ad261c6",
+    "url": "/webchat/static/js/main.40c8d8b6.chunk.js"
+  },
+  {
+    "revision": "6f4f34cbdcb8bf1f0574",
+    "url": "/webchat/static/js/runtime-main.757ea005.js"
+  }
+]);
